d60b0ea0ce835be6dd8cabdf1562620f0ab24e13
Jiang <10265570+JC6@users.noreply.github.com>
2018-03-30 10:32:10 +0800

    Chapter Five

--- chapter_05/assignment_5_2.py
+++ chapter_05/assignment_5_2.py
@@ -4,18 +4,17 @@ while True:
     num = input("Enter a number: ")
     if num == "done":
         break
-    elif largest is None:
-        largest = num
-    elif smallest is None:
-        smallest = num
     else:
         try:
-            if float(num) > float(largest):
-                largest = num
-            elif float(num) < float(smallest):
-                smallest = num
+            float(num)
         except ValueError:
             print("Invalid input")
-
+            continue
+    if largest is None or smallest is None:
+        largest = smallest = num
+    elif float(num) > float(largest):
+        largest = num
+    elif float(num) < float(smallest):
+        smallest = num
 print("Maximum is", largest)
 print("Minimum is", smallest)
