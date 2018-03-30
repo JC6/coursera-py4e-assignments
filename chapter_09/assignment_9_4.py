eff9361a2a5984579bb503602d40fcd1a8ed339a
Jiang <10265570+JC6@users.noreply.github.com>
2018-03-30 10:52:51 +0800

    Chapter Nine

--- chapter_09/assignment_9_4.py
+++ chapter_09/assignment_9_4.py
@@ -2,27 +2,19 @@ name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
-
 words = list()
 word_dict = dict()
 maximum = None
 maximum_count = 0
-
 for line in handle:
     if line.startswith("From"):
         split = line.split()
-        if len(split) < 3:
-            continue
-        word = split[1]
-        words.append(word)
-        if word in word_dict:
-            word_dict[word] += 1
-        else:
-            word_dict[word] = 1
-
+        if len(split) > 2:
+            word = split[1]
+            words.append(word)
+            word_dict[word] = word_dict.get(word, 0) + 1
 for word in words:
     if word_dict[word] > maximum_count:
         maximum = word
         maximum_count = word_dict[word]
-
 print(maximum, maximum_count)
