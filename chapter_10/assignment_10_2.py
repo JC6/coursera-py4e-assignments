1e28a3e7863a22964e5bcba92953f36f06e853a3
Jiang <10265570+JC6@users.noreply.github.com>
2018-03-30 10:54:51 +0800

    Chapter Ten

--- chapter_10/assignment_10_2.py
+++ chapter_10/assignment_10_2.py
@@ -2,22 +2,15 @@ name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
-
 words = list()
 word_dict = dict()
-
 for line in handle:
     if line.startswith("From") and len(line.split()) > 2:
         pos = line.find(":")
         word = line[pos - 2:pos]
         if word not in words:
             words.append(word)
-        if word in word_dict:
-            word_dict[word] += 1
-        else:
-            word_dict[word] = 1
-
+        word_dict[word] = word_dict.get(word, 0) + 1
 words.sort()
-
 for word in words:
     print(word, word_dict[word])
