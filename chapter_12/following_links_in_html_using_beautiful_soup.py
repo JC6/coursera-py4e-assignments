236209e801fdba5ad8bcaa6dce411131ca2b60e0
Jiang <10265570+JC6@users.noreply.github.com>
2018-03-30 11:17:38 +0800

    Chapter Twelve

--- chapter_12/following_links_in_html_using_beautiful_soup.py
+++ chapter_12/following_links_in_html_using_beautiful_soup.py
@@ -3,18 +3,14 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
-# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-
 url = input('Enter URL: ')
 count = int(input('Enter count: '))
 position = int(input('Enter position: '))
-
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
-
 i = 0
 while i < count - 1:
     tags = soup('a')
--- chapter_12/scraping_html_data_with_beautiful_soup.py
+++ chapter_12/scraping_html_data_with_beautiful_soup.py
@@ -3,25 +3,15 @@ from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 
-# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
-
-# html.parser is the HTML parser included in the standard Python 3 library.
-# information on other HTML parsers is here:
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
 soup = BeautifulSoup(html, "html.parser")
-
-# Retrieve all of the anchor tags
 tags = soup('span')
-count = 0
 comments_sum = 0
 for tag in tags:
-    count += 1
     comments_sum += int(tag.contents[0])
-print("Count ", count)
+print("Count ", len(tags))
 print("Sum ", comments_sum)
